0c8e230dcd39f7ba6ca1c69c04e3577bac7cfc2e
Ashrith Sheshan <ashrith.sheshan@nutanix.com>
2015-02-02 16:34:35 +0530

    Added basic DEPT config file

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$DEPT = array(
+
+		"CSE" =>"Computer Science and Engg" ,
+		"ISE" => "Information Science and Engg" ,
+		"ME" => "Mechanical Engg" ,
+		"CV" => "Civil Engg" ,
+		
+		
+	);
+//echo '<pre>';print_r($DEPT);echo '</pre>';
+	
+
+
+?>
