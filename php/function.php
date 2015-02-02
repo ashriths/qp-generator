6f19476f22cd5de638dbfc705db9bd3b598bb02d
Ashrith Sheshan <ashrith.sheshan@nutanix.com>
2015-02-02 16:35:37 +0530

    Updated dept name fetch and added pretty print

--- php/function.php
+++ php/function.php
@@ -1,7 +1,12 @@
 <?php
 require_once('dbconfig.php');
+require_once ('config.php');
 
-
+function prettyPrint($str){
+	echo '<pre>';
+	print_r($str);
+	echo '</pre>';
+}
 
 function setupDatabase(){
     $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
@@ -32,19 +37,14 @@ function getTableDetailsbyNonId($table,$att,$value)
   }
 
 function getFullDeptName($d){
-	$s = "Dept. of ";
-	switch ($d) {
-		case 'CSE':
-				return $s."Computer Science and Engineering";
-			
-		default:
-				return "";
-			
-
-
-	}
-	
+	global $DEPT;
+	if(array_key_exists($d, $DEPT))
+		return "Dept of ".$DEPT[$d];
+	else {
+		return '';
+	}	
 }
+
 
 function getCoursesBySemAndDept($sem,$dep)
   {
@@ -67,7 +67,24 @@ function getCoursesBySemAndDept($sem,$dep)
     return $result;
   }
 
-  function getQuestions($cid,$unit)
+  
+
+function getTableDetailsbyId($table,$att,$value){
+    $db = setupDatabase();
+    $sql = "SELECT * FROM $table WHERE $att = '$value'";
+   
+    $result = $db->query($sql);
+    if(!$result){ 
+
+      die('Error:'.$db->error);
+    }
+    
+    //by id cant return multiple rows ever cos its by id
+    $result = $result->fetch_assoc();
+    return $result;
+  }
+  
+function getQuestions($cid,$unit)
   {
     $db = setupDatabase();
     if(strlen($unit)==1)
@@ -90,21 +107,62 @@ function getCoursesBySemAndDept($sem,$dep)
     } 
     return $result;
   }
-
-function getTableDetailsbyId($table,$att,$value){
-    $db = setupDatabase();
-    $sql = "SELECT * FROM $table WHERE $att = '$value'";
-   
-    $result = $db->query($sql);
-    if(!$result){ 
-
+  
+function fetchResult($sql){
+	$db = setupDatabase();
+	$result = $db->query($sql);
+	if(!$result){ 
       die('Error:'.$db->error);
     }
+	$rows = array();
+	if(mysqli_num_rows($result)>1){
+    $rows = array();
+        while($row = $result->fetch_assoc()) {
+             $rows[] = $row;
+        }
     
-    //by id cant return multiple rows ever cos its by id
-    $result = $result->fetch_assoc();
-    return $result;
-  }
+    }else{
+    	$rows= $result->fetch_assoc(); 
+    }
+	//print_r($rows);
+	return array('count'=>mysqli_num_rows($result),'rows'=>$rows);
+	
+}
+
+
+
+function getRelatedQuestions($cid,$unit,$text){
+	$usual = array("explain","describe","example","what");
+	$words = split(' ', $text);
+	//$words = array_reverse($words);
+	$q = array();
+	foreach ($words as $word) {
+		if(!in_array($word, $usual)){
+			$sql = "SELECT * FROM question WHERE course_id = $cid AND unit = '$unit' AND text LIKE '%$word%'";
+			//echo $sql;
+			$result = fetchResult($sql);
+			if($result['count']>0){
+				foreach ($result['rows'] as $row) {
+					//print_r($row);
+					if(!isset($q[$row['question_id']])){
+						$q[$row['question_id']]= $row;	
+						$q[$row['question_id']]['match']=1;
+					}else{
+						$q[$row['question_id']]['match']++;	
+					}
+				}
+			}
+		}
+		
+	}
+	$match = array();
+	foreach ($q as $key => $value) {
+		$match[$key]=$value['match'];
+	}
+	array_multisort($match,SORT_DESC,$q);
+	//prettyPrint($q);	
+	return array('count'=>count($q),'rows'=>$q);  
+ }
 
  function addQuestion($c_id,$unit,$text,$marks,$co,$po,$img){
 		$db = setupDatabase();
